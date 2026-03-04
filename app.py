09abc9cc8a548f2a365d0096204d6eab364ab370
Buya Al-Fariz <buyafariz@gmail.com>
2026-03-04 10:57:55 +0700

    update

--- app.py
+++ app.py
@@ -1114,6 +1114,9 @@ with tab_cancel:
                         mime_type="application/vnd.google-apps.spreadsheet"
                     )
 
+                    st.write("Rows:", len(prod_log_df))
+                    st.write("Columns:", prod_log_df.columns.tolist())
+
                     try:
                         update_gsheet(
                             service=service,
