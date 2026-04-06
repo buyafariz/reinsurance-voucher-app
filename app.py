2c68c2e02cf2daeaa14bb164ecb5922e17e6a1aa
Buya Al-Fariz <buyafariz@gmail.com>
2026-04-06 12:08:34 +0700

    update

--- app.py
+++ app.py
@@ -837,6 +837,8 @@ with tab_upload:
                         row_dict=log_pml
                     )
 
+                    st.write(PML_DRIVE_ID)
+
                     upload_dataframe_to_drive(
                         service=service,
                         df=df,
