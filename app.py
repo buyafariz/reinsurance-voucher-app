fe0ae1a81edbb04b9d2c1d68117ff93c8b79b584
Buya Al-Fariz <buyafariz@gmail.com>
2026-04-13 21:54:46 +0700

    update

--- app.py
+++ app.py
@@ -875,7 +875,11 @@ with tab_calc:
         # ==========================
         # CEK RATE FILE
         # ==========================
-        selected_account = selected_rows.iloc[0]["Account With"]
+        if not selected_rows.empty:
+            selected_account = selected_rows.iloc[0]["Account With"]
+        else:
+            st.warning("Tidak ada data yang dipilih / ditemukan")
+            selected_account = None
 
         rate_file_id = find_drive_file(
             service=service,
